c810a3cbe56eadb6229e9378684ab347bfd805a2
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2025-02-09 21:19:24 -0500

    added tailwind, removed some files

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { Link, RouterProvider, createRouter } from '@tanstack/react-router'
+import './global.css'
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
