307378a42ce2b5c430eafdfc8f61cc1c774ed6c5
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2025-02-15 15:20:59 -0500

    sorted achievements

--- src/components/Tabs/Achievements/Achievements.tsx
+++ src/components/Tabs/Achievements/Achievements.tsx
@@ -12,49 +12,51 @@ const Achievements = () => {
   )
   return (
     <ul className="space-y-4 max-w-[600px]">
-      {achievements.map((achievement) => {
-        return (
-          <li
-            key={achievement.text}
-            className={clsx(achievement.visible ? 'visible' : 'hidden')}
-          >
-            <Card className="shadow-sm">
-              <div className="flex justify-between items-center gap-4">
-                <div>
-                  <Text
-                    wrap="pretty"
-                    className={clsx(
-                      achievement.achieved ? 'line-through' : 'line-none'
-                    )}
+      {achievements
+        .sort((a, b) => Number(a.achieved) - Number(b.achieved))
+        .map((achievement) => {
+          return (
+            <li
+              key={achievement.text}
+              className={clsx(achievement.visible ? 'visible' : 'hidden')}
+            >
+              <Card className="shadow-sm">
+                <div className="flex justify-between items-center gap-4">
+                  <div>
+                    <Text
+                      wrap="pretty"
+                      className={clsx(
+                        achievement.achieved ? 'line-through' : 'line-none'
+                      )}
+                    >
+                      {achievement.text}
+                    </Text>
+                  </div>
+                  <Tooltip
+                    content={achievement.achieved ? 'Achieved' : 'In progress'}
                   >
-                    {achievement.text}
-                  </Text>
+                    <button>
+                      {achievement.achieved ? (
+                        <CheckIcon
+                          width="25"
+                          height="25"
+                          color="green"
+                          aria-label="achieved"
+                        />
+                      ) : (
+                        <CountdownTimerIcon
+                          width="25"
+                          height="25"
+                          aria-label="in progress"
+                        />
+                      )}
+                    </button>
+                  </Tooltip>
                 </div>
-                <Tooltip
-                  content={achievement.achieved ? 'Achieved' : 'In progress'}
-                >
-                  <button>
-                    {achievement.achieved ? (
-                      <CheckIcon
-                        width="25"
-                        height="25"
-                        color="green"
-                        aria-label="achieved"
-                      />
-                    ) : (
-                      <CountdownTimerIcon
-                        width="25"
-                        height="25"
-                        aria-label="in progress"
-                      />
-                    )}
-                  </button>
-                </Tooltip>
-              </div>
-            </Card>
-          </li>
-        )
-      })}
+              </Card>
+            </li>
+          )
+        })}
     </ul>
   )
 }
