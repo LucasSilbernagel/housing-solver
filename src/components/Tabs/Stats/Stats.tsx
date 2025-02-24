1b69f9215b340ba7cc1934e61d2b344894d0d9c2
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2025-02-23 21:19:43 -0500

    setting up stats tab

--- src/components/Tabs/Stats/Stats.tsx
+++ src/components/Tabs/Stats/Stats.tsx
@@ -1,8 +1,50 @@
+import { Text } from '@radix-ui/themes'
+import { useShallow } from 'zustand/shallow'
+import { useGameStore } from '../../../hooks/use-game-store'
+
 const Stats = () => {
+  const { allTimePoints, automaticIncrementAmount, manualIncrementAmount } =
+    useGameStore(
+      useShallow((state) => ({
+        allTimePoints: state.allTimePoints,
+        automaticIncrementAmount: state.automaticIncrementAmount,
+        manualIncrementAmount: state.manualIncrementAmount,
+      }))
+    )
+
   return (
-    <div>
-      <p>Stats go here</p>
-    </div>
+    <ul className="space-y-12 max-h-none md:max-h-[450px] overflow-y-auto">
+      <li className="flex flex-col gap-2">
+        <div>
+          <Text size="4" wrap="pretty" className="font-bold">
+            Total support points generated:{' '}
+          </Text>
+        </div>
+        <div>
+          <Text>{allTimePoints.toLocaleString()}</Text>
+        </div>
+      </li>
+      <li className="flex flex-col gap-2">
+        <div>
+          <Text size="4" wrap="pretty" className="font-bold">
+            Support points automatically earned per second:{' '}
+          </Text>
+        </div>
+        <div>
+          <Text>{automaticIncrementAmount.toLocaleString()}</Text>
+        </div>
+      </li>
+      <li className="flex flex-col gap-2">
+        <div>
+          <Text size="4" wrap="pretty" className="font-bold">
+            Support points earned per click:{' '}
+          </Text>
+        </div>
+        <div>
+          <Text>{manualIncrementAmount.toLocaleString()}</Text>
+        </div>
+      </li>
+    </ul>
   )
 }
 
