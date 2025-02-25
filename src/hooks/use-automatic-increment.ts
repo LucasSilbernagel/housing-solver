13ef8ed5e76703023db32e9abf4d2f50233812ab
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2025-02-24 21:20:05 -0500

    stop automatic increment if reached maximum available points

--- src/hooks/use-automatic-increment.ts
+++ src/hooks/use-automatic-increment.ts
@@ -1,21 +1,30 @@
 import { useEffect } from 'react'
 import { useShallow } from 'zustand/shallow'
+import { MAXIMUM_SUPPORT_POINTS } from '../constants/stats'
 import { useGameStore } from './use-game-store'
 
 export const useAutomaticIncrement = () => {
-  const { automaticIncrementAmount, automaticallyIncrementPoints } =
-    useGameStore(
-      useShallow((state) => ({
-        automaticIncrementAmount: state.automaticIncrementAmount,
-        automaticallyIncrementPoints: state.automaticallyIncrementPoints,
-      }))
-    )
+  const {
+    automaticIncrementAmount,
+    automaticallyIncrementPoints,
+    availablePoints,
+  } = useGameStore(
+    useShallow((state) => ({
+      automaticIncrementAmount: state.automaticIncrementAmount,
+      automaticallyIncrementPoints: state.automaticallyIncrementPoints,
+      availablePoints: state.availablePoints,
+    }))
+  )
 
   useEffect(() => {
-    if (automaticIncrementAmount === 0) return
+    if (
+      automaticIncrementAmount === 0 ||
+      availablePoints === MAXIMUM_SUPPORT_POINTS
+    )
+      return
     const interval = setInterval(() => {
       automaticallyIncrementPoints()
     }, 1000)
     return () => clearInterval(interval)
-  }, [automaticIncrementAmount, automaticallyIncrementPoints])
+  }, [automaticIncrementAmount, automaticallyIncrementPoints, availablePoints])
 }
